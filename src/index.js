1c20ad45068611065a45198182514b23f20e5395
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2021-08-11 15:52:06 +0500

    removed unused dependencies and pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SimpleReactLightbox from 'simple-react-lightbox';
 import App from './App';
 
-ReactDOM.render(
-   <SimpleReactLightbox>
-      <App />
-   </SimpleReactLightbox>,
-   document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/jsx/components/charts/chartist/blank.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-function Blank() {
-   return <></>;
-}
-
-export default Blank;
